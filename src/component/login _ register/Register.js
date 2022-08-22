d92077cb173be8a87e10cded55d6326b04173027
ShiroKarma <85627794+ShiroKarma@users.noreply.github.com>
2022-08-22 13:48:01 +0700

    Update Register.js

--- src/component/login _ register/Register.js
+++ src/component/login _ register/Register.js
@@ -4,7 +4,7 @@ import liff from '@line/liff';
 import { useState, useEffect } from 'react';
 import './ContentCSS.css'
 import logo from '../../logo.svg'
-import { Redirect, useHistory } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import Cookies from 'js-cookie';
 function Register() {
   const navigate = useHistory();
@@ -65,7 +65,7 @@ function Register() {
         .then(() => {
           setLoader(false);
           alert("Register Complete");
-          <Redirect to="/camera"/>
+          <Link to="/camera"/>
         })
         .catch((error) => {
           alert(error.message);
